bb363cd4d84ab4d1fedc4009b34ad237a0613dcf
amanRio <108719073+amanRio@users.noreply.github.com>
2024-11-12 13:10:43 +0100

    editing build file adding import

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,11 +1,12 @@
 // vars/buildApp.groovy
+import com.example.utils.MessageFormatter
+
 def call(Map config = [:]) {
     stage('Build') {
         def profile = config.profile ?: 'dev'
         echo "Building the application with profile: ${profile}"
         def message = MessageFormatter.formatMessage("Deployment started", profile)
         echo message
-        sh "mvn clean install -P${profile}"
         
     }
 }
