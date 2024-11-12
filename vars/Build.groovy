b4737a50096f3e76a1368e04b5aea9eeeb25ed7d
amanRio <108719073+amanRio@users.noreply.github.com>
2024-11-12 12:56:36 +0100

    adding messeage formatter

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -3,6 +3,8 @@ def call(Map config = [:]) {
     stage('Build') {
         def profile = config.profile ?: 'dev'
         echo "Building the application with profile: ${profile}"
+        def message = MessageFormatter.formatMessage("Deployment started", profile)
+        echo message
         sh "mvn clean install -P${profile}"
     }
 }
